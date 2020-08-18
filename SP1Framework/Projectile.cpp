1d1c18d0be9121bfe1023a9fecbdd230740a8920
FMLneverlucky <66565733+FMLneverlucky@users.noreply.github.com>
2020-08-18 23:34:35 +0800

    now only shoots left, npcs no longer angery, someone figure out y projectile keeps falling downwards

--- SP1Framework/Projectile.cpp
+++ SP1Framework/Projectile.cpp
@@ -4,6 +4,8 @@ Projectile::Projectile()
 {
 	x = 40;
 	y = 0;
+	m_x = 0;
+	m_y = 0;
 	dir = none;
 }
 
@@ -17,6 +19,12 @@ void Projectile::set_ppos(int px, int py)
 	y = py;
 }
 
+void Projectile::set_mpos(int mx, int my)
+{
+	m_x = mx;
+	m_y = my;
+}
+
 void Projectile::update_particle()
 {
 	if (dir == left)
@@ -26,10 +34,10 @@ void Projectile::update_particle()
 		this->x += 1;
 
 	else if (dir == down)
-		this->y -= 1;
+		this->y += 1;
 
 	else
-		this->y += 1;
+		this->y -= 1;
 }
 
 int Projectile::get_px(void)
@@ -40,4 +48,26 @@ int Projectile::get_px(void)
 int Projectile::get_py(void)
 {
 	return y;
+}
+
+int Projectile::get_mx(void)
+{
+	return m_x;
+}
+
+int Projectile::get_my(void)
+{
+	return m_y;
+}
+
+void Projectile::direction()
+{
+	if (m_x < x)
+		dir = left;
+	else if (m_x > x)
+		dir = right;
+	else if (m_y < y)
+		dir = down;
+	else
+		dir = up;
 }
--- SP1Framework/Projectile.h
+++ SP1Framework/Projectile.h
@@ -11,16 +11,19 @@ enum face
 
 class Projectile
 {
-	int x, y;
+	int x, y, m_x, m_y;
 	face dir;
 
 public:
 	Projectile();
 	~Projectile();
 	void set_ppos(int px, int py);
+	void set_mpos(int mx, int my);
 	void update_particle();
 	int get_px(void);
 	int get_py(void);
+	int get_mx(void);
+	int get_my(void);
 	void direction();
 };
 
